a634e944f3a1b64b1a47871d1c03a7990d29fd87
Zildjian Garcia <zackyization@gmail.com>
2025-12-01 21:14:54 +0800

    Add an error page - Add asset files for the 404 error not found page - Rename 404's page.tsx file to not-found.tsx

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,4 +1,3 @@
-// TODO: The error 404 page for when a user navigates to a non-existent route
 import type { Metadata } from "next";
 import Link from "next/link";
 import Image from "next/image";
@@ -11,21 +10,24 @@ export const metadata: Metadata = {
 export default function NotFound() {
   return (
     <div className="flex min-h-[calc(100vh-6rem)] flex-col items-center justify-center px-6 text-center">
-      <div className="relative mb-8 h-64 w-full max-w-xl">
+      <div className="mb-8 h-64 w-full max-w-xl relative">
         <Image
-          src="/assets/404/salamander.png"
-          alt="404 illustration"
+          src="/assets/error_hippo_dogs.svg"
+          alt="Multiver and his fellow hippodogs"
           fill
           className="object-contain"
         />
       </div>
 
-      <h1 className="text-5xl font-extrabold">404</h1>
-      <p className="mt-2 text-lg text-secondary-text">Page not found</p>
+      <h1 className="text-5xl font-extrabold text-tertiary-text">404</h1>
+      <p className="mt-2 text-xl font-outfit font-bold text-tertiary-text">
+        Page not found
+      </p>
 
       <Link
         href="/"
-        className="mt-6 rounded-full bg-primary px-6 py-3 text-sm font-semibold hover:brightness-110"
+        className="mt-6 rounded-lg bg-black px-6 py-3 text-sm font-semibold hover:bg-black/40 transition-colors"
+        title="Home Page"
       >
         Go to the Home Page
       </Link>
